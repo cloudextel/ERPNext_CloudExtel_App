6aac53f7b7a0a3bdbb3c0037352507c9d417adf0
Samarth Upare <s.upare@cloudextel.com>
2024-02-29 23:07:22 +0530

    changes

--- cloudextel/create.py
+++ cloudextel/create.py
@@ -11,18 +11,19 @@ def increment_last_two_digits(string):
 @frappe.whitelist()
 def perform_task():
     try:
+        month = 2
         sqls = """
         SELECT DISTINCT p.name AS pname
         FROM `tabDocument Naming Rule` AS p
         INNER JOIN `tabDocument Naming Rule Condition` AS c  
         ON c.parent = p.name
         WHERE c.field = 'posting_month' 
-        AND c.value = 2
+        AND c.value = %(month)s
         AND p.document_type = 'Sales Invoice'
         AND SUBSTRING(p.prefix, -4, 2) = '24' 
         AND p.disabled = 0
         """
-        desp = frappe.db.sql(sqls, as_dict=1)
+        desp = frappe.db.sql(sqls,{'month':month}, as_dict=1)
         print(desp)
 
         for row in desp:
@@ -32,12 +33,12 @@ def perform_task():
             new_doc.document_type = doc.document_type
             new_doc.priority = doc.priority
             new_doc.prefix_digits = doc.prefix_digits 
-            new_doc.disabled = 1
+            doc.disabled = 1
             
             
             # Modify conditions
             for child in new_doc.conditions:
-                if child.field == 'posting_month' and int(child.value) == 1:
+                if child.field == 'posting_month' and int(child.value) == month:
                     child.value =  int(child.value)+1
 
             # Reset counter and increment prefix
@@ -48,7 +49,7 @@ def perform_task():
                 new_doc.prefix = strig[:-2] + str(int(strig[-2:]) + 1).zfill(2) if len(strig) >= 2 and strig[-2:].isdigit() else strig  
             
             # Insert new document and commit changes
-            new_doc.insert()
+            new_doc.save()
             frappe.db.commit()
     except Exception as e:
         print("Error:", e)
@@ -68,3 +69,49 @@ def check():
 
 
 
+# def perform_task():
+#     try:
+#         month = 2
+#         sqls = """
+#         SELECT DISTINCT p.name AS pname
+#         FROM `tabDocument Naming Rule` AS p
+#         INNER JOIN `tabDocument Naming Rule Condition` AS c  
+#         ON c.parent = p.name
+#         WHERE c.field = 'posting_month' 
+#         AND c.value = %(month)s
+#         AND p.document_type = 'Sales Invoice'
+#         AND SUBSTRING(p.prefix, -4, 2) = '24' 
+#         AND p.disabled = 0
+#         """
+#         desp = frappe.db.sql(sqls,{'month':month}, as_dict=1)
+        
+
+#         for row in desp:
+#             doc = frappe.get_doc('Document Naming Rule', row['pname'])
+#             new_doc = frappe.new_doc('Document Naming Rule')
+#             new_doc.conditions = doc.conditions
+#             new_doc.document_type = doc.document_type
+#             new_doc.priority = doc.priority
+#             new_doc.prefix_digits = doc.prefix_digits 
+            
+            
+#             # Modify conditions
+#             for child in new_doc.conditions:
+#                 if child.field == 'posting_month' and int(child.value) == month:
+#                     child.value =  int(child.value)+1
+
+#             # Reset counter and increment prefix
+#             new_doc.counter = 0
+
+#             if doc.prefix:
+#                 strig = doc.prefix
+#                 new_doc.prefix = strig[:-2] + str(int(strig[-2:]) + 1).zfill(2) if len(strig) >= 2 and strig[-2:].isdigit() else strig  
+            
+#             # Insert new document and commit changes
+#             new_doc.save()
+#             frappe.db.commit()
+#     except Exception as e:
+#         log(e)
+
+
+# perform_task()   
